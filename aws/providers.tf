88493e49f7abb45388e48f69448fa2f430de2865
Matt Johnson <matt@bridgecrew.io>
2021-06-16 15:39:59 +0100

    Save default s3 state provider config

--- aws/providers.tf
+++ aws/providers.tf
@@ -4,15 +4,10 @@ provider "aws" {
   region  = var.region
 }
 
-provider "aws" {
-  alias      = "plain_text_access_keys_provider"
-  region     = "us-west-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-}
-
 terraform {
   backend "s3" {
-    encrypt = true
+    bucket = "deployablegoat-test"
+    key    = "s3state/deployablegoat"
+    region = var.region
   }
 }
