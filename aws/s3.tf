2231b73198b848a2d4475fe6dbfab852a27f1005
Matt Johnson <matt+github@metahertz.co.uk>
2024-11-14 22:05:38 +0000

    Yor.io Tagging

--- aws/s3.tf
+++ aws/s3.tf
@@ -7,9 +7,17 @@ resource "aws_s3_bucket" "data" {
   acl           = "public-read"
   force_destroy = true
   tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-    yor_trace   = "0c23f4fd-6fd9-4ed1-925f-6c1b13388837"
+    Name                 = "${local.resource_prefix.value}-data"
+    Environment          = local.resource_prefix.value
+    yor_trace            = "0c23f4fd-6fd9-4ed1-925f-6c1b13388837"
+    git_commit           = "8aefc18757655c5685aca4a12b2a1a179f620ee1"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2021-06-16 14:25:48"
+    git_last_modified_by = "matt@bridgecrew.io"
+    git_modifiers        = "matt"
+    git_org              = "metahertz"
+    git_repo             = "deployablegoat"
+    yor_name             = "data"
   }
 }
 
@@ -21,9 +29,17 @@ resource "aws_s3_bucket" "financials" {
   acl           = "private"
   force_destroy = true
   tags = {
-    Name        = "${local.resource_prefix.value}-financials"
-    Environment = local.resource_prefix.value
-    yor_trace   = "b1450859-3ab4-43db-b287-c19034fe0fbb"
+    Name                 = "${local.resource_prefix.value}-financials"
+    Environment          = local.resource_prefix.value
+    yor_trace            = "b1450859-3ab4-43db-b287-c19034fe0fbb"
+    git_commit           = "8aefc18757655c5685aca4a12b2a1a179f620ee1"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2021-06-16 14:25:48"
+    git_last_modified_by = "matt@bridgecrew.io"
+    git_modifiers        = "matt"
+    git_org              = "metahertz"
+    git_repo             = "deployablegoat"
+    yor_name             = "financials"
   }
 }
 
@@ -37,9 +53,17 @@ resource "aws_s3_bucket" "operations" {
   }
   force_destroy = true
   tags = {
-    Name        = "${local.resource_prefix.value}-operations"
-    Environment = local.resource_prefix.value
-    yor_trace   = "4534313c-6d0d-457d-b928-4c89689fb08a"
+    Name                 = "${local.resource_prefix.value}-operations"
+    Environment          = local.resource_prefix.value
+    yor_trace            = "4534313c-6d0d-457d-b928-4c89689fb08a"
+    git_commit           = "8aefc18757655c5685aca4a12b2a1a179f620ee1"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2021-06-16 14:25:48"
+    git_last_modified_by = "matt@bridgecrew.io"
+    git_modifiers        = "matt"
+    git_org              = "metahertz"
+    git_repo             = "deployablegoat"
+    yor_name             = "operations"
   }
 
 }
